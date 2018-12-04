45507e2c6e90266beb05a57960f6f59b5fa01eca
Lapo La Fata <lafata.lapo@gmail.com>
2018-12-04 10:07:08 +0100

    Quarto Commit

--- ProgettoCompitoLaFata/Origine.cpp
+++ ProgettoCompitoLaFata/Origine.cpp
@@ -6,7 +6,7 @@ void main()
 	int iconta = 0; //Creazione variabile contatore
 	for (int i = 0; i < 10; i++) //ciclo per far scorrere variabile
 	{
-		iconta++;
+		iconta++;//incremento variabile contatore
 		cout << "Prova:" <<iconta << endl;
 		
 	}
